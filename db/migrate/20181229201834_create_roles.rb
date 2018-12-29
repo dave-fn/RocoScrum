928cfba7458b36f60e47e184a7958c62bee77876
David Negrete <davidfnegrete@gmail.com>
2018-12-29 13:26:42 -0700

    Updated table parameters

--- db/migrate/20181229201834_create_roles.rb
+++ db/migrate/20181229201834_create_roles.rb
@@ -1,10 +1,10 @@
 class CreateRoles < ActiveRecord::Migration[5.2]
   def change
     create_table :roles do |t|
-      t.string :name
-      t.string :description
-      t.integer :min_participants
-      t.integer :max_participants
+      t.string :name, null: false
+      t.string :description, null: false
+      t.integer :min_participants, null: false, default: 1
+      t.integer :max_participants, null: false, default: 1
 
       t.timestamps
     end
