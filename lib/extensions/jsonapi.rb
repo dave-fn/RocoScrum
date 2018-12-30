f0fd68fa0c0c9300d945b8ae522a70b742f14fa9
David Negrete <davidfnegrete@gmail.com>
2018-12-29 18:49:32 -0700

    Remove API version from generated links in response

---
+++ lib/extensions/jsonapi.rb
@@ -0,0 +1,19 @@
+# Monkey Patch / Hack
+module JSONAPI
+  class LinkBuilder
+
+    private
+
+    def formatted_module_path_from_class(klass)
+      scopes = module_scopes_from_class(klass)
+
+      unless scopes.empty?
+        "/#{scopes.map(&:underscore).join('/')}/".gsub(/api\/v[0-9]+\//, 'api/')
+      else
+        "/"
+      end
+      
+    end
+
+  end
+end
