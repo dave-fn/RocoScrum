c0d2d5d997201beefae2c74d513f55d4c54445b2
David Negrete <davidfnegrete@gmail.com>
2018-12-30 13:43:01 -0700

    Moved token route to api

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 require 'api_constraint'
 
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
 
   namespace :api do
+    post 'user_token' => 'user_token#create'
     scope module: :v1, constraints: ApiConstraint.new(version: 1, default: false) do
       jsonapi_resources :roles do
       end
     end
   end
+  
 end
