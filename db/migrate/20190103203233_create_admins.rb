fb495f8fc39ff612a590a458c609559c297c6934
David Negrete <davidfnegrete@gmail.com>
2019-01-03 13:35:53 -0700

    Updated table parameters

--- db/migrate/20190103203233_create_admins.rb
+++ db/migrate/20190103203233_create_admins.rb
@@ -1,7 +1,7 @@
 class CreateAdmins < ActiveRecord::Migration[5.2]
   def change
     create_table :admins do |t|
-      t.references :user, foreign_key: true
+      t.references :user, foreign_key: true, index: {unique: true}
       t.datetime :last_logged_at
 
       t.timestamps
