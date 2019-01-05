2b5662c3c3568b77f27f4032e8addc59125757dc
David Negrete <davidfnegrete@gmail.com>
2019-01-04 22:34:52 -0700

    Partial implementation of Policy specs

--- spec/policies/role_policy_spec.rb
+++ spec/policies/role_policy_spec.rb
@@ -1,27 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe RolePolicy do
-  let(:user) { User.new }
 
-  subject { described_class }
+  subject { described_class.new user, described_class }
+
+  let!(:scrum_master_role)  { create(:scrum_master_role) }
+  let!(:product_owner_role)  { create(:product_owner_role) }
+  let!(:developer_role)  { create(:developer_role) }
+
+  context 'as unauthenticated user' do
+    let(:user)  { nil }
+
+    it { is_expected.to permit_action :index }
+    it { is_expected.to permit_action :show }
+
+    it { is_expected.to forbid_new_and_create_actions }
+    it { is_expected.to forbid_edit_and_update_actions }
+    it { is_expected.to forbid_action :destroy }
+  end
+
+  context 'as authenticated user' do
+    let(:user) { create :dummy_user }
+
+    it { is_expected.to permit_action :index }
+    it { is_expected.to permit_action :show }
+
+    it { is_expected.to forbid_new_and_create_actions }
+    it { is_expected.to forbid_edit_and_update_actions }
+    it { is_expected.to forbid_action :destroy }
+  end
 
   permissions ".scope" do
     pending "add some examples to (or delete) #{__FILE__}"
   end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
 end
--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -1,27 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe UserPolicy do
-  let(:user) { User.new }
 
-  subject { described_class }
+  subject { described_class.new user, described_class }
+
+  let(:all_users)  { create_list :dummy_user, 5 }
+
+  context 'as unauthenticated user' do
+    let(:user)  { nil }
+
+    it { is_expected.to permit_action :index }
+    it { is_expected.to permit_action :show }
+
+    it { is_expected.to forbid_new_and_create_actions }
+    it { is_expected.to forbid_edit_and_update_actions }
+    it { is_expected.to forbid_action :destroy }
+  end
+
+  context 'as authenticated user' do
+    let(:user) { create :dummy_user }
+
+    it { is_expected.to permit_action :index }
+    it { is_expected.to permit_action :show }
+
+    it { is_expected.to forbid_new_and_create_actions }
+    it { is_expected.to forbid_edit_and_update_actions }
+    it { is_expected.to forbid_action :destroy }
+  end
 
   permissions ".scope" do
     pending "add some examples to (or delete) #{__FILE__}"
   end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
 end
