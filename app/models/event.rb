ec4ef94ef2df9b57bd436e03e4fc548cba7f5e03
David Negrete <davidfnegrete@gmail.com>
2019-01-10 20:53:31 -0700

    Defined specs, implemented, and created factories

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,8 @@
 class Event < ApplicationRecord
+
+  include Hashid::Rails
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :timebox, presence: true, numericality: { greater_than_or_equal_to: 1 }
+
 end
