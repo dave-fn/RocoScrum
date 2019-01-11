9852e0fcc0fe7d7654770684a29dea1889247afd
David Negrete <davidfnegrete@gmail.com>
2019-01-10 20:07:06 -0700

    Defined specs for Resources' scopes

--- spec/policies/role_policy_spec.rb
+++ spec/policies/role_policy_spec.rb
@@ -4,9 +4,12 @@ RSpec.describe RolePolicy do
 
   subject { described_class.new user, described_class }
 
+  let(:resolved_scope)  { described_class::Scope.new(user, Role.all).resolve }
+
   let!(:scrum_master_role)  { create(:scrum_master_role) }
   let!(:product_owner_role)  { create(:product_owner_role) }
   let!(:developer_role)  { create(:developer_role) }
+  let(:scrum_roles)  { [scrum_master_role, product_owner_role, developer_role] }
 
   context 'as unauthenticated user' do
     let(:user)  { nil }
@@ -30,8 +33,22 @@ RSpec.describe RolePolicy do
     it { is_expected.to forbid_action :destroy }
   end
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
+  permissions '.scope' do
+    context 'as unauthenticated user' do
+      let(:user)  { nil }
+
+      it 'includes all roles' do
+        expect(resolved_scope).to eq scrum_roles
+      end
+    end
+
+    context 'as authenticated user' do
+      let(:user)  { create :dummy_user }
+
+      it 'includes all roles' do
+        expect(resolved_scope).to eq scrum_roles
+      end
+    end
   end
 
 end
--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe UserPolicy do
 
   subject { described_class.new user, described_class }
 
-  let(:all_users)  { create_list :dummy_user, 5 }
+  let(:resolved_scope)  { described_class::Scope.new(user, User.all).resolve }
+
+  let!(:all_users)  { create_list :dummy_user, 5 }
 
   context 'as unauthenticated user' do
     let(:user)  { nil }
@@ -28,8 +30,22 @@ RSpec.describe UserPolicy do
     it { is_expected.to forbid_action :destroy }
   end
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
+  permissions '.scope' do
+    context 'as unauthenticated user' do
+      let(:user)  { nil }
+
+      it 'includes all users' do
+        expect(resolved_scope).to eq all_users
+      end
+    end
+
+    context 'as authenticated user' do
+      let(:user)  { create :dummy_user }
+
+      it 'includes all users' do
+        expect(resolved_scope).to include *all_users
+      end
+    end
   end
 
 end
