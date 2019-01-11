b2cad30e9b82de7bce53c0646501f68d08a2ba1a
David Negrete <davidfnegrete@gmail.com>
2019-01-11 14:11:46 -0700

    Defined Policy specs

--- app/policies/project_policy.rb
+++ app/policies/project_policy.rb
@@ -1,7 +1,37 @@
 class ProjectPolicy < ApplicationPolicy
+
+  def index?
+    authenticated_user?
+  end
+
+  def show?
+    owned_by_user?
+  end
+
+  def create?
+    authenticated_user?
+  end
+
+  def update?
+    owned_by_user?
+  end
+
+  def destroy?
+    owned_by_user?
+  end
+
+
+  private
+
+  def owned_by_user?
+    record.admin == user
+  end
+
+
   class Scope < Scope
     def resolve
-      scope.all
+      scope.where(admin: user)
     end
   end
+
 end
