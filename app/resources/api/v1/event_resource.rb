0e89f2f5ca908068162118fe1b622e9335577af3
David Negrete <davidfnegrete@gmail.com>
2019-01-10 21:41:43 -0700

    Improve timebox output

--- app/resources/api/v1/event_resource.rb
+++ app/resources/api/v1/event_resource.rb
@@ -9,4 +9,11 @@ class Api::V1::EventResource < Api::V1::ResourceBase
   
   filter :name
 
+  def timebox
+    t = @model.timebox
+    tvalues = [t/86400, t/3600%24, t/60%60, t%60]
+    return ActiveSupport::Duration.new(nil, month: 1).inspect if tvalues[0] >= 30
+    ActiveSupport::Duration.new(nil, days: tvalues[0], hours: tvalues[1], minutes: tvalues[2], seconds: tvalues[3]).inspect
+  end
+
 end
