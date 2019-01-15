e11ee823334582db5ef5315e873478cc129f6e96
David Negrete <davidfnegrete@gmail.com>
2019-01-15 16:10:45 -0700

    Added User Policy permitted attributes

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -7,10 +7,18 @@ class UserPolicy < ApplicationPolicy
   def show?
     true
   end
+
+
+  # Strong Parameters
+  def permitted_attributes
+    [:name, :email, :password, :password_confirmation, :projects]
+  end
+
   
   class Scope < Scope
     def resolve
       scope.all
     end
   end
+
 end
