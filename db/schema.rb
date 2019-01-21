d53a5b6d7b17f16176f6bd72c701ee951c1d7897
David Negrete <davidfnegrete@gmail.com>
2019-01-21 16:34:20 -0700

    Executed rails db:create

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_11_192556) do
+ActiveRecord::Schema.define(version: 2019_01_21_230627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 2019_01_11_192556) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "teams", force: :cascade do |t|
+    t.bigint "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_teams_on_project_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
@@ -60,4 +67,5 @@ ActiveRecord::Schema.define(version: 2019_01_11_192556) do
 
   add_foreign_key "admins", "users"
   add_foreign_key "projects", "users", column: "admin_id"
+  add_foreign_key "teams", "projects"
 end
