536cbd41804fe797b2a0f6290c549faf817e634e
David Negrete <davidfnegrete@gmail.com>
2019-02-01 17:14:55 -0700

    Added logic to verify max limit ot developers in team

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,9 @@ class Team < ApplicationRecord
 
   include Hashid::Rails
 
+  # validates :developers, length: {minimum: Role.developer.min_participants, maximum: Role.developer.max_participants }
+  # validates :developers, length: {minimum: 1, maximum: 9, message: "instances outside min/max values."}
+
   belongs_to :project, inverse_of: :teams
 
   # All Members
@@ -14,10 +17,34 @@ class Team < ApplicationRecord
   
   # Developers
   has_many :developers_membership, -> { developers }, class_name: 'TeamMembership'
-  has_many :developers, through: :developers_membership, source: :user
+  has_many :developers, through: :developers_membership, source: :user, before_add: :limit_adding_developers
 
   def size
     team_memberships.size
   end
+
+
+  private
+
+  def limit_adding_developers(developer)
+    too_many_developers! if developers.size >= max_developers
+  end
+
+  def max_developers
+    @max_developers ||= Role.developer.max_participants
+  end
+
+  module Errors
+    class MaxDevelopers < StandardError; end
+  end
+
+  def too_many_developers!
+    raise Errors::MaxDevelopers, "Limited to at most #{max_developers} developers"
+  end
+
+  # def count_team_developers
+  #   errors.add(:developers, :too_short, count: 3) if developers.size < 3
+  #   errors.add(:developers, :too_long, count: 9) if developers.size > 9
+  # end
 
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -11,8 +11,62 @@ RSpec.describe Team, type: :model do
   it { should have_one(:scrum_master).source(:user) }
   it { should have_many(:developers).source(:user) }
 
-  describe '#size' do
-    pending 'test size with dummy team, create factory to ease creation of teams'
+
+  context 'concrete instances' do
+    let(:team)  { create :working_team, developer_count: developer_count }
+    let!(:roles)  { create :scrum_master_role; create :developer_role, max_participants: 7 }
+
+    describe '#size' do
+      let(:developer_count)  { 7 }
+
+      it 'counts all members' do
+        expect(team.size).to eq 8
+      end
+    end
+
+    describe '#developers' do
+      context 'under minimum' do
+        let(:developer_count)  { 0 }
+
+        it 'returns team developers' do
+          expect(team.developers.size).to eq 0
+        end
+      end
+      
+      context 'within limits' do
+        let(:developer_count)  { 5 }
+
+        it 'returns team developers' do
+          expect(team.developers.size).to eq 5
+        end
+      end
+    end
+
+
+    context 'when validating relationship' do
+
+      context 'developers' do
+        context 'under minimum' do
+          let(:developer_count)  { 0 }
+
+          specify { expect(team).to be_valid }
+          pending 'set flag to mark non-compliant as intermediate state -- aside from valid'
+        end
+
+        context 'within limits' do
+          let(:developer_count)  { 5 }
+
+          specify { expect(team).to be_valid }
+        end
+
+        context 'over maximum' do
+          let(:developer_count)  { 10 }
+
+          specify { expect { team }.to raise_error Team::Errors::MaxDevelopers }
+        end
+      end
+
+    end
   end
 
 end
