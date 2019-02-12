8d79d6fcf5820a42e7b4c10978eab410f91bd67c
David Negrete <davidfnegrete@gmail.com>
2019-02-12 15:45:30 -0700

    Updated migration

--- db/migrate/20190212224144_create_products.rb
+++ db/migrate/20190212224144_create_products.rb
@@ -3,8 +3,8 @@ class CreateProducts < ActiveRecord::Migration[5.2]
     create_table :products do |t|
       t.string :title
       t.text :description
-      t.references :owner, foreign_key: true
-      t.references :project, foreign_key: true
+      t.references :owner, foreign_key: {to_table: :users}
+      t.references :project, foreign_key: true, index: true
 
       t.timestamps
     end
