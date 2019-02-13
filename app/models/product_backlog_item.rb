0bba094356996a57b794ac37e9ae269a68a781e9
David Negrete <davidfnegrete@gmail.com>
2019-02-13 14:18:28 -0700

    Added ProductBacklogItem uniqueness validations

--- app/models/product_backlog_item.rb
+++ app/models/product_backlog_item.rb
@@ -5,4 +5,7 @@ class ProductBacklogItem < ApplicationRecord
   belongs_to :product, inverse_of: :product_backlog_items
   belongs_to :backlog_item, inverse_of: :product_backlog_item
 
+  validates :product_id, uniqueness: {scope: [:backlog_item_id]}
+  validates :backlog_item_id, uniqueness: {scope: [:product_id]}
+
 end
--- spec/models/product_backlog_item_spec.rb
+++ spec/models/product_backlog_item_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe ProductBacklogItem, type: :model do
   it { should belong_to(:product).inverse_of(:product_backlog_items) }
   it { should belong_to(:backlog_item).inverse_of(:product_backlog_item) }
 
+
+  describe 'Uniqueness validations' do
+    subject  { build :product_backlog_item }
+
+    it { should validate_uniqueness_of(:product_id).scoped_to(:backlog_item_id) }
+    it { should validate_uniqueness_of(:backlog_item_id).scoped_to(:product_id) }
+  end
+
 end
