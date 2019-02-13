2410d73bd8cefc0e09896eb0c9a0acc2934c8a03
David Negrete <davidfnegrete@gmail.com>
2019-02-13 13:02:58 -0700

    Executed rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_041904) do
+ActiveRecord::Schema.define(version: 2019_02_13_200040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,16 @@ ActiveRecord::Schema.define(version: 2019_02_13_041904) do
     t.string "description", null: false
     t.integer "min_participants", default: 1, null: false
     t.integer "max_participants", default: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sprints", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description"
+    t.text "goal"
+    t.integer "duration"
+    t.datetime "started_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
