43b985e04aee7f6ddd94422e8810734b4c1fdf21
David Negrete <davidfnegrete@gmail.com>
2019-02-13 14:22:15 -0700

    Updated migration

--- db/migrate/20190213211906_add_composite_index_to_product_backlog_item.rb
+++ db/migrate/20190213211906_add_composite_index_to_product_backlog_item.rb
@@ -1,4 +1,6 @@
 class AddCompositeIndexToProductBacklogItem < ActiveRecord::Migration[5.2]
   def change
+    add_index :product_backlog_items, [:product_id, :backlog_item_id], unique: true
+    add_index :product_backlog_items, [:backlog_item_id, :product_id], unique: true
   end
 end
