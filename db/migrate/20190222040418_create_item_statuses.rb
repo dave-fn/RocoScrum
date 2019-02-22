28a1d59743beba03b81b6db6ffec344246ec570f
David Negrete <davidfnegrete@gmail.com>
2019-02-21 21:06:50 -0700

    Updated migration

--- db/migrate/20190222040418_create_item_statuses.rb
+++ db/migrate/20190222040418_create_item_statuses.rb
@@ -2,9 +2,11 @@ class CreateItemStatuses < ActiveRecord::Migration[5.2]
   def change
     create_table :item_statuses do |t|
       t.references :team, foreign_key: true
-      t.string :context
-      t.string :title
+      t.string :context, limit: 5, null: false
+      t.string :title, null: false
       t.string :description
+
+      t.index [:team_id, :context, :title], unique: true
 
       t.timestamps
     end
