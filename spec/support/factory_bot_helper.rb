e047f9b94674eee7b5978e9b8274edba23a7f178
David Negrete <davidfnegrete@gmail.com>
2019-02-21 20:59:40 -0700

    Updated FactoryBotHelper

--- spec/support/factory_bot_helper.rb
+++ spec/support/factory_bot_helper.rb
@@ -5,15 +5,26 @@ module FactoryBotHelper
   class << self
 
     def roles_factories
-      roles = _roles_factories
-      roles.each { |factory| yield(factory) } if block_given?
-      roles
+      _each_factories(_roles_factories)
+    end
+
+    def sprint_backlog_item_statuses_factories
+      _each_factories(_sbi_statuses_factories)
     end
 
     private
 
+    def _each_factories(factories)
+      factories.each { |factory| yield(factory) } if block_given?
+      factories
+    end
+
     def _roles_factories
       [:scrum_master_role, :developer_role, :product_owner_role]
+    end
+
+    def _sbi_statuses_factories
+      [:sbi_committed_status, :sbi_assigned_status, :sbi_in_progress_status, :sbi_completed_status, :sbi_pending_status]
     end
 
   end
