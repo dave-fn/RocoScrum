37e64deb813518d35c7716ea69220ff7cd89e5c4
David Negrete <davidfnegrete@gmail.com>
2019-02-21 22:02:12 -0700

    Updated migration

--- db/migrate/20190222050429_create_sbi_status_updates.rb
+++ db/migrate/20190222050429_create_sbi_status_updates.rb
@@ -3,8 +3,8 @@ class CreateSbiStatusUpdates < ActiveRecord::Migration[5.2]
     create_table :sbi_status_updates do |t|
       t.references :sprint_backlog_item, foreign_key: true
       t.references :item_status, foreign_key: true
-      t.references :developer, foreign_key: true
-      t.references :creator, foreign_key: true
+      t.references :developer, foreign_key: {to_table: :users}, index: true
+      t.references :creator, foreign_key: {to_table: :users}, index: true
 
       t.timestamps
     end
